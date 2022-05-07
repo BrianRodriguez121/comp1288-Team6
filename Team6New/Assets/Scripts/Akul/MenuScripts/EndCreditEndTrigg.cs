a0112778a2b002c2f8071393208377a796e1f09b
AkulOmg <67987040+AkulOmg@users.noreply.github.com>
2022-05-07 04:05:44 +0100

    End Credit scene Compleated
    Final End credit scene completed with all triggers and Final Assets imported to scene.

---
+++ Team6New/Assets/Scripts/Akul/MenuScripts/EndCreditEndTrigg.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCreditEndTrigg : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            SceneManager.LoadScene(0);
+        }
+    }
+
+
+    
+
+
+}
+
+    
+
+
+
+
+
